0289451ff15ddd6e9e7a04d69aeb5cbbed7043a3
cuongnn <c>
2021-08-18 16:45:32 +0700

    update s3Api

--- vue_sony/src/s3Api/index.js
+++ vue_sony/src/s3Api/index.js
@@ -1,20 +1,19 @@
 import AWS from "aws-sdk";
-class S3Api {
-  constructor() {
+export default class S3Api {
+  constructor(file) {
+    var fileName = file.name;
+    var albumPhotosKey = "creative_works/";
+    var photoKey = albumPhotosKey + fileName;
     AWS.config.region = "ap-northeast-1";
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
       IdentityPoolId: "ap-northeast-1:f9f79180-8602-4034-a400-8db839a07c45",
     });
+    return new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: "bc-secure-storage-api-cuongnn-bucket83908e77-nczm2ffo15wh",
+        Key: photoKey,
+        Body: file,
+      },
+    });
   }
-  upload = new AWS.S3.ManagedUpload({
-    params: {
-      Bucket: "bc-secure-storage-api-cuongnn-bucket83908e77-nczm2ffo15wh",
-      Key: photoKey,
-      Body: file,
-    },
-  });
 }
-
-const api = new S3Api();
-
-export default api;
